48ddf2c504c541a7d5cad2d2800edbfd79757b81
ekiwan <elie.kiwan@gmail.com>
2013-08-02 13:14:02 -0700

    added the case to handle an array. nothing passes yet because the code to handle individual elements hasn't been written yet

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,16 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  var str;
+  if (Array.isArray(obj)) { //handle the case of obj being an array
+    str = '[';
+    for (var i = 0; i < obj.length; i++) {
+      str = str + stringifyJSON(obj[i]); //send each element back into the function
+      if (obj.length > 0 && i < (obj.length - 1)) { //adds commas up to the last element
+        str = str + ',';
+      }
+      }
+    str = str + ']' //close off the array
+  }
+  return str;
 };
